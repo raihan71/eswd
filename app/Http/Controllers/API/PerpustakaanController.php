b258c497b38755fc6b4e92ed094fa43485f41261
Ichsan Maulana <michsanm1999@gmail.com>
2018-10-04 23:02:03 +0700

    getBuku()
    - Add Addbuku Logic and Output

--- app/Http/Controllers/API/PerpustakaanController.php
+++ app/Http/Controllers/API/PerpustakaanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Book;
 
 class PerpustakaanController extends Controller
 {
@@ -12,19 +13,24 @@ class PerpustakaanController extends Controller
     	return view('perpustakaan.index');
     }
 
-    public function getBuku(){
+    public function getBuku(Request $request){
+      $data = $request->all();
+      $buku = Book::getBuku($data);
+
+      return response()->json($buku);
 
     }
+
     public function getBukuDetail(){
 
     }
-    
+
     public function storeBuku(){
 
     }
-    
+
     public function storePinjam(){
 
     }
-    
+
 }
